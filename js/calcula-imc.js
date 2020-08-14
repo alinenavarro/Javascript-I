cc362466787151bc7493f8f3b199b356a2614fd0
Aline Martineli De Araujo Navarro <aline.navarro@totvs.com.br>
2020-08-13 22:03:08 -0300

    adicionadas validações no formulário

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -15,26 +15,38 @@ for (var i = 0; i < pacientes.length; i++) {
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoEhValido = true;
-    var alturaEhValida = true;
+    var pesoValid = validaPeso(peso); 
+    var alturaValid = validaAltura(altura);
 
-    if (peso <= 0 || peso >= 1000) {
-        console.log("Peso inválido!");
-        pesoEhValido = false;
-        tdImc.textContent = "Peso inválido";
+    if (!pesoValid) {
+        tdImc.textContent = "Peso inválido ";
         paciente.classList.add("paciente-invalido");
     }
 
-    if (altura <= 0 || altura >= 3.00) {
-        console.log("Altura inválida!");
-        alturaEhValida = false;
-        tdImc.textContent = "Altura inválida";
+    if (!alturaValid) {
+        tdImc.textContent = "Altura inválida ";
         paciente.classList.add("paciente-invalido");
     }
 
-    if (pesoEhValido && alturaEhValida) {
+    if (pesoValid && alturaValid) {
         var imc = calculaImc(peso,altura);
         tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso <1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.00){
+        return true;
+    }else{
+        return false;
     }
 }
 
--- js/form.js
+++ js/form.js
@@ -7,15 +7,34 @@ botaoAdicionar.addEventListener("click", function(event){
     var paciente = obtemPacienteForm(form);
     
     var pacienteTr = montaTr(paciente);
-   
+    
+    var erros = validaPaciente(paciente);
+
+    if(erros.length > 0){
+       msgErro(erros);
+       return;
+    }
+
     // Adiciona paciente na tabela
     var tabela = document.querySelector("#tabela-pacientes");
 
     tabela.appendChild(pacienteTr);
 
     form.reset();
+    var mensagensErro = document.querySelector("#mensagens-erro");
+    mensagensErro.innerHTML = "";
 
 });
+
+function msgErro(erros){
+    var ul = document.querySelector("#mensagens-erro");
+    ul.innerHTML = "";
+    erros.forEach(function(erro){
+        var li = document.createElement("li");
+        li.textContent = erro;
+        ul.appendChild(li);
+    });
+}
 
  //Extrai informacoes do paciente do form
 function obtemPacienteForm(form){
@@ -48,3 +67,34 @@ function montaTd(dado,classe){
     td.classList.add(classe);
     return td;
 }
+
+function validaPaciente(paciente){
+
+    var erros = [];
+
+    if (paciente.nome.length == 0){
+        erros.push("Nome não pode ser em branco");
+    }
+
+     if( paciente.peso.length == 0){
+         erros.push("Peso não pode ser em branco");
+     }
+
+     if( paciente.altura.length == 0){
+        erros.push("Altura não pode ser em branco");
+    }
+
+    if( paciente.gordura.length == 0){
+        erros.push("Gordura não pode ser em branco");
+    }
+
+    if(!validaPeso(paciente.peso)){
+        erros.push("Peso inválido");
+     }
+
+    if(!validaAltura(paciente.altura)){
+        erros.push("Altura inválida");
+     }
+     return erros;
+}
+
